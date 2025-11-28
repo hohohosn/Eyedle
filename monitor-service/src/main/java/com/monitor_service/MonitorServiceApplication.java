3667956e2dfb9aefe899af6653ede16bad13fb1f
hohohosn <day7of01@gmail.com>
2025-11-28 15:07:10 +0900

    feat(gateway):라우팅 및 Eureka 설정 추가

--- monitor-service/src/main/java/com/monitor_service/MonitorServiceApplication.java
+++ monitor-service/src/main/java/com/monitor_service/MonitorServiceApplication.java
@@ -2,8 +2,10 @@ package com.monitor_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MonitorServiceApplication {
 
 	public static void main(String[] args) {
