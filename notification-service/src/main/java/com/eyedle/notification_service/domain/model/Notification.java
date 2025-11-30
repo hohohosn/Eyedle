384e4ebc5ec997d0c87adecea1ccbf68ba4bf253
kimpompu <domathing@naver.com>
2025-11-30 20:35:30 +0900

    feat:ADD notification-service 엔티티 추가
    - Notification, NotificationType 엔티티 추가

---
+++ notification-service/src/main/java/com/eyedle/notification_service/domain/model/Notification.java
@@ -0,0 +1,87 @@
+package com.eyedle.notification_service.domain.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.github.f4b6a3.tsid.TsidCreator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "p_notification")
+public class Notification {
+	@Id
+	@Column(name = "notification_id")
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "notification_type", nullable = false)
+	private NotificationType notificationType;
+
+	@Column(name = "target_id", nullable = false)
+	private Long targetId;
+
+	@Column(name = "sub_target_id")
+	private Long subTargetId;
+
+	@Column(name = "receiver_id", nullable = false)
+	private Long receiverId;
+
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String message;
+
+	@Column(name = "read_at")
+	private LocalDateTime readAt;
+
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+
+	@PrePersist
+	public void prePersist() {
+		if(this.id == null){
+			this.id = TsidCreator.getTsid().toLong();
+		}
+	}
+
+	@Builder
+	public Notification(NotificationType type
+		, Long targetId
+		, Long subTargetId
+		, Long receiverId
+		, String message) {
+		this.notificationType = type;
+		this.targetId = targetId;
+		this.subTargetId = subTargetId;
+		this.receiverId = receiverId;
+		this.message = message;
+	}
+
+	public void markAsRead() {
+		this.readAt = LocalDateTime.now();
+	}
+
+	public void softDelete() {
+		this.deletedAt = LocalDateTime.now();
+	}
+
+}
