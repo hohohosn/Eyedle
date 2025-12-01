a06d1e2b958bd8eda141d3e1319e78aeaac4568c
soyoon <152174972+soyxni@users.noreply.github.com>
2025-12-01 15:56:26 +0900

    feat: 검색 서비스 도메인 구현(Document) #3

---
+++ search-service/src/main/java/com/search_service/domain/model/UserDocument.java
@@ -0,0 +1,29 @@
+package com.search_service.domain.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(indexName = "users")
+public class UserDocument {
+
+	@Id
+	private Long id;
+
+	@Field(type = FieldType.Text, analyzer = "nori")
+	private String userId;
+
+	@Field(type = FieldType.Keyword, index = false)
+	private String profileImageUrl;
+}
