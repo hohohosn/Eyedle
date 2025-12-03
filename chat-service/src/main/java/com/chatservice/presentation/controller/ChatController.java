1968678107454363c8f2df307bbbcd77d1e009cd
guswnee00 <guswnee00@gmail.com>
2025-12-03 15:17:41 +0900

    add: 채팅 수락/거절 controller #5

--- chat-service/src/main/java/com/chatservice/presentation/controller/ChatController.java
+++ chat-service/src/main/java/com/chatservice/presentation/controller/ChatController.java
@@ -1,12 +1,14 @@
 package com.chatservice.presentation.controller;
 
 import static com.common.response.SuccessCode.CREATED;
+import static com.common.response.SuccessCode.OK;
 
 import com.chatservice.application.service.ChatService;
 import com.chatservice.presentation.request.CreateChatRoomReqDto;
 import com.chatservice.presentation.response.CreateChatRoomResDto;
 import com.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,20 @@ public class ChatController {
   public CommonResponse<CreateChatRoomResDto> createDirectChatRoom(@RequestBody CreateChatRoomReqDto reqDto) {
     Long userId = 1L;
     return CommonResponse.of(CREATED, chatService.createDirectChatRoom(userId, reqDto));
+  }
+
+  @PostMapping("/{chatRoomId}/accept")
+  public CommonResponse<Void> acceptChatRoom(@PathVariable Long chatRoomId) {
+    Long userId = 1L;
+    chatService.acceptChatRoom(chatRoomId, userId);
+    return CommonResponse.of(OK, null);
+  }
+
+  @PostMapping("/{chatRoomId}/reject")
+  public CommonResponse<Void> rejectChatRoom(@PathVariable Long chatRoomId) {
+    Long userId = 1L;
+    chatService.rejectChatRoom(chatRoomId, userId);
+    return CommonResponse.of(OK, null);
   }
 }
 
