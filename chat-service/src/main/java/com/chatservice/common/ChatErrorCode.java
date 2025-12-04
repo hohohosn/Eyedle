7d87523d39bb4b49f1f9796c80c1dda77bec754c
guswnee00 <guswnee00@gmail.com>
2025-12-05 01:02:11 +0900

    add: 메세지 관련 에러코드 추가 #5

--- chat-service/src/main/java/com/chatservice/common/ChatErrorCode.java
+++ chat-service/src/main/java/com/chatservice/common/ChatErrorCode.java
@@ -15,6 +15,12 @@ public enum ChatErrorCode implements BaseErrorCode {
   DUPLICATE_CHAT_PARTICIPANT("CHAT1001", "중복된 채팅 참여자입니다.", 409),
   ALREADY_LEFT_CHAT_ROOM("CHAT1002", "이미 채팅방을 나간 상태입니다.", 409),
 
+  // ChatMessage
+  CHAT_MESSAGE_NOT_FOUND("CHAT2000", "채팅 메세지를 찾을 수 없습니다.", 404),
+  MESSAGE_NOT_IN_CHATROOM("CHAT2001", "이 채팅방의 메세지가 아닙니다.", 400),
+  NOT_MESSAGE_OWNER("CHAT2002", "다른 사용자의 메세지는 삭제할 수 없습니다.", 403),
+  MESSAGE_ALREADY_DELETED("CHAT2003", "이미 삭제된 메세지입니다.", 400),
+
   // USER
   USER_NOT_FOUND("CHAT5000", "사용자를 찾을 수 없습니다.", 404),
   BLOCKED_USER("CHAT5001", "차단 상태입니다.", 403),
