64101667a0e2794cf918f33a3d9bde2a53c1688f
guswnee00 <guswnee00@gmail.com>
2025-12-04 16:51:54 +0900

    add: 에러 코드 추가 및 status 타입 변경 #5

--- chat-service/src/main/java/com/chatservice/common/ChatErrorCode.java
+++ chat-service/src/main/java/com/chatservice/common/ChatErrorCode.java
@@ -1,29 +1,29 @@
 package com.chatservice.common;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-
 import com.common.response.BaseErrorCode;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ChatErrorCode implements BaseErrorCode {
 
   // ChatRoom
-  CHAT_ROOM_NOT_FOUND("CHAT0000", "채팅방을 찾을 수 없습니다.", NOT_FOUND),
-  SELF_CHAT_NOT_ALLOWED("CHAT0001", "자기 자신과의 채팅방은 만들 수 없습니다.", BAD_REQUEST),
+  CHAT_ROOM_NOT_FOUND("CHAT0000", "채팅방을 찾을 수 없습니다.", 404),
+  SELF_CHAT_NOT_ALLOWED("CHAT0001", "자기 자신과의 채팅방은 만들 수 없습니다.", 400),
 
   // ChatParticipate
-  CHAT_PARTICIPATE_NOT_FOUND("CHAT0010", "채팅 참여자를 찾을 수 없습니다.", NOT_FOUND),
+  CHAT_PARTICIPATE_NOT_FOUND("CHAT1000", "채팅 참여자를 찾을 수 없습니다.", 404),
+  DUPLICATE_CHAT_PARTICIPATE("CHAT1001", "중복된 채팅 참여자입니다.", 409),
 
+  // USER
+  USER_NOT_FOUND("CHAT5000", "사용자를 찾을 수 없습니다.", 404),
+  BLOCKED_USER("CHAT5001", "차단 상태입니다.", 403),
   ;
 
   private final String code;
   private final String message;
-  private final HttpStatus status;
+  private final int status;
 
-  ChatErrorCode(String code, String message, HttpStatus status) {
+  ChatErrorCode(String code, String message, int status) {
     this.code = code;
     this.message = message;
     this.status = status;
