20244d271b79cf0405453bebf26573b6987b64ef
minsik0 <qwew1212@naver.com>
2025-12-04 21:10:45 +0900

    [refactor] Feed 코드 리팩토링

--- feed-service/src/main/java/com/feed_service/domain/model/Feed.java
+++ feed-service/src/main/java/com/feed_service/domain/model/Feed.java
@@ -1,5 +1,6 @@
 package com.feed_service.domain.model;
 
+import com.common.database.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,13 +11,9 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "p_feed")
-@Where(clause = "is_deleted = false")
 @Getter
 @NoArgsConstructor
-public class Feed {
-
-    @Id
-    private Long id; // TSID
+public class Feed extends BaseTimeEntity {
 
     @Column(nullable = false)
     private Long userId;
@@ -28,33 +25,23 @@ public class Feed {
     @Column(nullable = false)
     private FeedPermission permission;
 
-    private boolean isDeleted;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
     private LocalDateTime deletedAt;
 
     @Builder
-    public Feed(Long id, Long userId, String content, FeedPermission permission,
-                boolean isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
-        this.id = id;
+    public Feed(Long userId, String content, FeedPermission permission) {
         this.userId = userId;
         this.content = content;
         this.permission = permission;
-        this.isDeleted = isDeleted;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
     }
 
-    // 수정
     public void updateFeed(String content, FeedPermission permission) {
         this.content = content;
         this.permission = permission;
-        this.updatedAt = LocalDateTime.now();
     }
 
-    // 논리삭제
     public void statusDeleted() {
         this.isDeleted = true;
         this.deletedAt = LocalDateTime.now();
