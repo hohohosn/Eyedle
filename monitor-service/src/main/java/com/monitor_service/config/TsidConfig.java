9f06e58cc98aff48c7593ff573ab9061d6b1181a
soyoon <152174972+soyxni@users.noreply.github.com>
2025-12-04 23:05:11 +0900

    feat: TSID 노드 번호 설정

---
+++ monitor-service/src/main/java/com/monitor_service/config/TsidConfig.java
@@ -0,0 +1,21 @@
+package com.monitor_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import com.common.utils.TsidUtil;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class TsidConfig {
+
+	@Value("${instance.node-id}")
+	private int nodeId;
+
+	@PostConstruct
+	public void init(){
+		TsidUtil.setNodeId(nodeId);
+		System.out.println(">>> TSID Node ID initialized: " + nodeId);
+	}
+}
