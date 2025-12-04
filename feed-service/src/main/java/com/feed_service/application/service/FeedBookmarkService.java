8f6ecab4cb1bb9a8566df12d25a094d9d3407319
minsik0 <qwew1212@naver.com>
2025-12-04 21:06:10 +0900

    [refactor] FeedBookmarkService 코드 리팩토링

--- feed-service/src/main/java/com/feed_service/application/service/FeedBookmarkService.java
+++ feed-service/src/main/java/com/feed_service/application/service/FeedBookmarkService.java
@@ -1,7 +1,11 @@
 package com.feed_service.application.service;
 
+import com.common.exception.CustomException;
+import com.common.response.ErrorCode;
+import com.feed_service.domain.model.Feed;
 import com.feed_service.domain.model.FeedBookmark;
 import com.feed_service.domain.repository.FeedBookmarkRepository;
+import com.feed_service.domain.repository.FeedRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,18 +13,24 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class FeedBookmarkService {
 
+    private final FeedRepository feedRepository;
     private final FeedBookmarkRepository feedBookmarkRepository;
 
     @Transactional
     public void bookmarkFeed(Long feedId, Long userId){
-        boolean exists = feedBookmarkRepository.existsByFeedIdAndUserId(feedId, userId);
-        if(exists) throw new IllegalStateException("이미 저장한 게시글");
 
-        feedBookRepository.save(new FeedBookmark(feedId, userId));
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(() -> new CustomException(ErrorCode.CONFLICT));
+
+        if(feedBookmarkRepository.existsByFeed_IdAndUserId(feedId, userId)){
+            throw new CustomException(ErrorCode.CONFLICT);
+        }
+
+        feedBookmarkRepository.save(new  FeedBookmark(feed, userId));
     }
 
     @Transactional
     public void deleteBookmarkFeed(Long feedId, Long userId){
-        feedBookRepository.deleteByFeedIdAndUserId(feedId, userId);
+        feedBookmarkRepository.deleteByFeed_IdAndUserId(feedId, userId);
     }
 }
