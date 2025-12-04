855588e82521a635d0692cbd353841b179982cc3
minsik0 <qwew1212@naver.com>
2025-12-04 21:08:28 +0900

    [fix] FeedBookmarkRepository 코드 수정

--- feed-service/src/main/java/com/feed_service/domain/repository/FeedBookmarkRepository.java
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedBookmarkRepository.java
@@ -1,5 +1,11 @@
 package com.feed_service.domain.repository;
 
-public interface FeedBookmarkRepository {
-    boolean existsByFeedIdAndUserId(Long feedId, Long userId);
+import com.feed_service.domain.model.FeedBookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedBookmarkRepository extends JpaRepository<FeedBookmark, Long> {
+
+    boolean existsByFeed_IdAndUserId(Long feedId, Long userId);
+
+    void deleteByFeed_IdAndUserId(Long feedId, Long userId);
 }
