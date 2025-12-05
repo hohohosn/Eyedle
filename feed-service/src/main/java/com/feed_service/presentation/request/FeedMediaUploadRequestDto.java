88d00dfef2a7962906ed5f0bbf0db6816a53f14f
minsik0 <qwew1212@naver.com>
2025-12-05 21:23:44 +0900

    add: FeedMediaUploadRequestDto 추가

---
+++ feed-service/src/main/java/com/feed_service/presentation/request/FeedMediaUploadRequestDto.java
@@ -0,0 +1,10 @@
+package com.feed_service.presentation.request;
+
+import com.feed_service.domain.model.MediaType;
+
+public class FeedMediaUploadRequestDto {
+
+    private String url;
+    private MediaType mediaType;
+
+}
