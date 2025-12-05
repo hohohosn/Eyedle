87a6ef04dcd452c08a24ce492564a2f9b9208131
minsik0 <qwew1212@naver.com>
2025-12-05 21:24:28 +0900

    fix: FeedResponseDto 코드 수정

--- feed-service/src/main/java/com/feed_service/presentation/response/FeedResponseDto.java
+++ feed-service/src/main/java/com/feed_service/presentation/response/FeedResponseDto.java
@@ -1,8 +1,11 @@
 package com.feed_service.presentation.response;
 
 import com.feed_service.domain.model.Feed;
+import com.feed_service.domain.model.FeedMedia;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -12,6 +15,7 @@ public class FeedResponseDto {
     private Long userId;
     private String content;
     private String permission;
+    private List<FeedMedia> media;
 
     public static FeedResponseDto mapFeed(Feed feed) {
         return FeedResponseDto.builder()
