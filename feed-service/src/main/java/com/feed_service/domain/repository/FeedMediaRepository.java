d7af6ab51cb03cbc0b93a870a890daa7b40a363d
minsik0 <qwew1212@naver.com>
2025-12-05 22:06:19 +0900

    fix: FeedMediaRepository 코드 수정

--- feed-service/src/main/java/com/feed_service/domain/repository/FeedMediaRepository.java
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedMediaRepository.java
@@ -3,5 +3,8 @@ package com.feed_service.domain.repository;
 import com.feed_service.domain.model.FeedMedia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedMediaRepository extends JpaRepository<FeedMedia, Long> {
+    List<FeedMedia> findByFeedId(Long feedId);
 }
