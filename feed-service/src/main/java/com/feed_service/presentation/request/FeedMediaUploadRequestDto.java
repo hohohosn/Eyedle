0a3741e38a0aebe8493acacc2dd1787a486055df
minsik0 <qwew1212@naver.com>
2025-12-06 04:25:54 +0900

    fix: FeedMediaUploadRequestDto 코트 수정

--- feed-service/src/main/java/com/feed_service/presentation/request/FeedMediaUploadRequestDto.java
+++ feed-service/src/main/java/com/feed_service/presentation/request/FeedMediaUploadRequestDto.java
@@ -1,10 +1,14 @@
 package com.feed_service.presentation.request;
 
 import com.feed_service.domain.model.MediaType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class FeedMediaUploadRequestDto {
 
-    private String url;
+    private String mediaUrl;
     private MediaType mediaType;
 
 }
