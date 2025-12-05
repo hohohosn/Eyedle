e1437e3ae729b9c3f3bfde0fe0dc486f756a9ca7
guswnee00 <guswnee00@gmail.com>
2025-12-05 15:49:42 +0900

    fix: 쿼리 최적화 #5

--- chat-service/src/main/java/com/chatservice/infra/repository/impl/ChatMessageRepositoryImpl.java
+++ chat-service/src/main/java/com/chatservice/infra/repository/impl/ChatMessageRepositoryImpl.java
@@ -1,17 +1,18 @@
 package com.chatservice.infra.repository.impl;
 
 import com.chatservice.domain.model.ChatMessage;
+import com.chatservice.domain.model.QChatMessage;
 import com.chatservice.domain.repository.ChatMessageRepository;
 import com.chatservice.infra.repository.jpa.ChatMessageJpaRepository;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import static com.chatservice.domain.model.QChatMessage.chatMessage;
 
 @Repository
 @RequiredArgsConstructor
@@ -32,11 +33,21 @@ public class ChatMessageRepositoryImpl implements ChatMessageRepository {
 
   @Override
   public Map<Long, ChatMessage> findLastMessageByChatRoomIds(List<Long> chatRoomIds) {
-    List<ChatMessage> messages = jpaQueryFactory
-        .selectFrom(chatMessage)
-        .where(chatMessage.chatRoomId.in(chatRoomIds))
-        .orderBy(chatMessage.chatRoomId.asc(), chatMessage.createdAt.desc())
+    QChatMessage m1 = new QChatMessage("m1");
+    QChatMessage m2 = new QChatMessage("m2");
+
+    List<ChatMessage> lastMessages = jpaQueryFactory
+        .select(m1)
+        .from(m1)
+        .where(m1.createdAt.eq(
+                JPAExpressions
+                    .select(m2.createdAt.max())
+                    .from(m2)
+                    .where(m2.chatRoomId.eq(m1.chatRoomId)))
+            .and(m1.chatRoomId.in(chatRoomIds)))
+        .orderBy(m1.chatRoomId.asc(), m1.id.desc())
         .fetch();
-    return messages.stream().collect(Collectors.toMap(ChatMessage::getChatRoomId, msg -> msg, (existing, replacement) -> existing));
+
+    return lastMessages.stream().collect(Collectors.toMap(ChatMessage::getChatRoomId, Function.identity()));
   }
 }
