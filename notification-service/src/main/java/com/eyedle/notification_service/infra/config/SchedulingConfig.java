97a2c3c042b80c2d04aef3516e4be0801ad5ebf4
kimpompu <domathing@naver.com>
2025-12-05 15:40:17 +0900

    add : 30일이 지난 알림 삭제 Batch 구현
    - 발송 30일이 지난 알림들을 매일 새벽 3시에 일괄 삭제하는 Batch 구현

---
+++ notification-service/src/main/java/com/eyedle/notification_service/infra/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.eyedle.notification_service.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
