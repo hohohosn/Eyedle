762f7e0d6eb76618d821c746993afb606d768aca
minsik0 <qwew1212@naver.com>
2025-12-06 22:07:28 +0900

    add: FeedBookmarkController 추가

---
+++ feed-service/src/main/java/com/feed_service/presentation/controller/FeedBookmarkController.java
@@ -0,0 +1,29 @@
+package com.feed_service.presentation.controller;
+
+import com.common.response.CommonResponse;
+import com.common.response.SuccessCode;
+import com.feed_service.application.service.FeedBookmarkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/feeds/{feedId}/bookmarks")
+public class FeedBookmarkController {
+
+    private final FeedBookmarkService feedBookmarkService;
+
+    @PostMapping
+    public CommonResponse bookmark(@PathVariable Long feedId) {
+        Long userId = 1L;
+        feedBookmarkService.bookmarkFeed(feedId, userId);
+        return CommonResponse.of(SuccessCode.CREATED);
+    }
+
+    @DeleteMapping
+    public CommonResponse unbookmark(@PathVariable Long feedId) {
+        Long userId = 1L;
+        feedBookmarkService.bookmarkFeed(feedId, userId);
+        return CommonResponse.of(SuccessCode.DELETED);
+    }
+}
