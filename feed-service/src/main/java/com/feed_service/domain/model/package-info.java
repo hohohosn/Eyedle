abfacf40bb049ee90869b95894a10dd4c0f65e5d
kimpompu <domathing@naver.com>
2025-12-09 15:37:40 +0900

    add : package-info 파일
    추가

---
+++ feed-service/src/main/java/com/feed_service/domain/model/package-info.java
@@ -0,0 +1,6 @@
+@QueryEntities({BaseTimeEntity.class, BaseRootEntity.class})
+package com.feed_service.domain.model;
+
+import com.common.database.BaseRootEntity;
+import com.common.database.BaseTimeEntity;
+import com.querydsl.core.annotations.QueryEntities;
