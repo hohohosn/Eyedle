823dae690498a4d71dc0e88078ac652f7b90208c
hohohosn <day7of01@gmail.com>
2025-12-09 12:42:06 +0900

    fix : 메서드 충돌 수정 #4

--- user-service/src/main/java/com/user_service/presentation/dto/response/UserResponse.java
+++ user-service/src/main/java/com/user_service/presentation/dto/response/UserResponse.java
@@ -33,7 +33,7 @@ public class UserResponse {
 			.username(user.getUsername())
 			.role(user.getRole())
 			.status(user.getStatus())
-			.deleted(user.isDeleted())
+			.deleted(user.deleted())
 			.createdAt(user.getCreatedAt())
 			.createdBy(user.getCreatedBy())
 			.updatedAt(user.getUpdatedAt())
