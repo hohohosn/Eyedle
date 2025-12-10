02a1bc0ee99b421c83c9d3cb0788ccbff8fbbfd9
minsik0 <qwew1212@naver.com>
2025-12-10 21:00:47 +0900

    del: S3 더미데이터 클래스 삭제

--- feed-service/src/main/java/com/feed_service/infra/s3/S3Uploader.java
+++
@@ -1,8 +0,0 @@
-package com.feed_service.infra.s3;
-
-public interface S3Uploader {
-
-    String upload(org.springframework.web.multipart.MultipartFile file, String directory);
-
-    void delete(String fileUrl);
-}
