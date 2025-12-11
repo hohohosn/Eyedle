210413ff8ad1fea2e9d35f6e2fa44459b66a4c33
minsik0 <qwew1212@naver.com>
2025-12-12 02:33:10 +0900

    fix: FeedLikeService toggle 방식으로 코드 변경

--- feed-service/src/main/java/com/feed_service/application/service/FeedLikeService.java
+++ feed-service/src/main/java/com/feed_service/application/service/FeedLikeService.java
@@ -2,10 +2,12 @@ package com.feed_service.application.service;
 
 import com.common.exception.CustomException;
 import com.common.response.ErrorCode;
+import com.common.response.SuccessCode;
 import com.feed_service.domain.model.Feed;
 import com.feed_service.domain.model.FeedLike;
 import com.feed_service.domain.repository.FeedLikeRepository;
 import com.feed_service.domain.repository.FeedRepository;
+import com.feed_service.presentation.response.FeedLikeResponseDto;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,19 +20,21 @@ public class FeedLikeService {
     private final FeedRepository feedRepository;
     private final FeedLikeRepository feedLikeRepository;
 
-    public void createLike(Long feedId, Long userId) {
+    public FeedLikeResponseDto toggleLike(Long feedId, Long feedLikeId) {
 
         Feed feed = feedRepository.findById(feedId)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
 
-        if(feedLikeRepository.existsByFeed_IdAndUserId(feedId, userId)){
-            throw new CustomException(ErrorCode.CONFLICT);
+        boolean exists = feedLikeRepository.existsByFeed_IdAndUserId(feedId, feedLikeId);
+
+        if(exists) {
+            feedLikeRepository.deleteByFeed_IdAndUserId(feedId, feedLikeId);
+        } else{
+            feedLikeRepository.save(new FeedLike(feed, feedLikeId));
         }
 
-        feedLikeRepository.save(new FeedLike(feed, userId));
-    }
+        long likeCount = feedLikeRepository.countByFeed_Id(feedId);
 
-    public void deleteLike(Long feedId, Long userId) {
-        feedLikeRepository.deleteByFeed_IdAndUserId(feedId, userId);
+        return new FeedLikeResponseDto(!exists, likeCount);
     }
 }
