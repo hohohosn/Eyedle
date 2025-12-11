26eee8fc0fc330a4d0fc2e714be1da355f9bb59f
minsik0 <qwew1212@naver.com>
2025-12-12 02:34:42 +0900

    add: FeedLikeRepository 코드 추가

--- feed-service/src/main/java/com/feed_service/domain/repository/FeedLikeRepository.java
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedLikeRepository.java
@@ -1,12 +1,18 @@
 package com.feed_service.domain.repository;
 
-import com.feed_service.domain.model.Feed;
 import com.feed_service.domain.model.FeedLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
 
     boolean existsByFeed_IdAndUserId(Long feedId, Long userId);
 
     void deleteByFeed_IdAndUserId(Long feedId, Long userId);
+
+    long countByFeed_Id(Long feedId);
+
+    List<FeedLike> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
+
