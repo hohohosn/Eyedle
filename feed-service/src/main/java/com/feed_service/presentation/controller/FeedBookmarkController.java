85316bae31e513b92e4506e42c4ef285143e5358
minsik0 <qwew1212@naver.com>
2025-12-12 02:37:35 +0900

    fix: FeedBookmarkController 토글 방식으로 변경

--- feed-service/src/main/java/com/feed_service/presentation/controller/FeedBookmarkController.java
+++ feed-service/src/main/java/com/feed_service/presentation/controller/FeedBookmarkController.java
@@ -3,6 +3,7 @@ package com.feed_service.presentation.controller;
 import com.common.response.CommonResponse;
 import com.common.response.SuccessCode;
 import com.feed_service.application.service.FeedBookmarkService;
+import com.feed_service.presentation.response.FeedBookmarkResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,16 +15,10 @@ public class FeedBookmarkController {
     private final FeedBookmarkService feedBookmarkService;
 
     @PostMapping
-    public CommonResponse bookmark(@PathVariable Long feedId) {
+    public CommonResponse toggleBookmark(@PathVariable Long feedId) {
         Long userId = 1L;
-        feedBookmarkService.bookmarkFeed(feedId, userId);
-        return CommonResponse.of(SuccessCode.CREATED);
-    }
+        FeedBookmarkResponseDto responseDto = feedBookmarkService.toggleBookmakr(userId, feedId);
 
-    @DeleteMapping
-    public CommonResponse unbookmark(@PathVariable Long feedId) {
-        Long userId = 1L;
-        feedBookmarkService.deleteBookmarkFeed(feedId, userId);
-        return CommonResponse.of(SuccessCode.DELETED);
+        return CommonResponse.of(SuccessCode.OK, responseDto);
     }
 }
