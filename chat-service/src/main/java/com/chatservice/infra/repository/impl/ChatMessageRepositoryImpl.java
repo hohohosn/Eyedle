260f607ee91e8a4b5840efd1ec3c4ab8e35bfd80
guswnee00 <guswnee00@gmail.com>
2025-12-11 12:49:19 +0900

    fix: 서브 쿼리 방식으로 변경 #5

--- chat-service/src/main/java/com/chatservice/infra/repository/impl/ChatMessageRepositoryImpl.java
+++ chat-service/src/main/java/com/chatservice/infra/repository/impl/ChatMessageRepositoryImpl.java
@@ -1,8 +1,10 @@
 package com.chatservice.infra.repository.impl;
 
 import com.chatservice.domain.model.ChatMessage;
+import com.chatservice.domain.model.QChatMessage;
 import com.chatservice.domain.repository.ChatMessageRepository;
 import com.chatservice.infra.repository.jpa.ChatMessageJpaRepository;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -37,15 +39,20 @@ public class ChatMessageRepositoryImpl implements ChatMessageRepository {
   @Override
   public Map<Long, ChatMessage> findLastMessageByChatRoomIds(List<Long> chatRoomIds) {
 
+    QChatMessage m1 = new QChatMessage("m1");    // 가져올 메시지
+    QChatMessage m2 = new QChatMessage("m2");    // 서브 쿼리용 메시지 -> 각 채팅방의 최신 메시지 시각 계산
+
     List<ChatMessage> lastMessages = jpaQueryFactory
-        .select(chatMessage)
-        .from(chatMessage)
-        .where(chatMessage.chatRoomId.in(chatRoomIds))
-        .orderBy(
-            chatMessage.chatRoomId.asc(),     // DISTINCT ON 기준
-            chatMessage.createdAt.desc()      // 최신 메세지 정렬
-        )
-        .distinct()
+        .select(m1)
+        .from(m1)
+        // 각 채팅방에서 가장 최신 메시지인지 확인
+        .where(m1.createdAt.eq(                   // m1의 메시지 중에서 서브쿼리에서 나온 최대 시간과 같은 메시지 선택
+                JPAExpressions
+                    .select(m2.createdAt.max())   // m1과 같은 채팅방 안에서 나온 가장 최신 시간
+                    .from(m2)
+                    .where(m2.chatRoomId.eq(m1.chatRoomId)))
+            .and(m1.chatRoomId.in(chatRoomIds)))
+        .orderBy(m1.chatRoomId.asc(), m1.id.desc())
         .fetch();
 
     return lastMessages.stream().collect(Collectors.toMap(ChatMessage::getChatRoomId, Function.identity()));
