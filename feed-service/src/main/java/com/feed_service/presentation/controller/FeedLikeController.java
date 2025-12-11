084097c5946365f86b75fd1ecae4458f4174e2d2
minsik0 <qwew1212@naver.com>
2025-12-12 02:32:42 +0900

    fix: FeedLikeController toggle 방식으로 코드 변경

--- feed-service/src/main/java/com/feed_service/presentation/controller/FeedLikeController.java
+++ feed-service/src/main/java/com/feed_service/presentation/controller/FeedLikeController.java
@@ -1,9 +1,12 @@
 package com.feed_service.presentation.controller;
 
 import com.common.response.CommonResponse;
+import com.common.response.ErrorCode;
 import com.common.response.SuccessCode;
 import com.feed_service.application.service.FeedLikeService;
+import com.feed_service.domain.model.FeedBookmark;
 import com.feed_service.domain.model.FeedLike;
+import com.feed_service.presentation.response.FeedLikeResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,16 +18,10 @@ public class FeedLikeController {
     private final FeedLikeService feedLikeService;
 
     @PostMapping
-    public CommonResponse like(@PathVariable Long feedId) {
+    public CommonResponse toggleLike(@PathVariable Long feedId){
         Long userId = 1L;
-        feedLikeService.createLike(feedId, userId);
-        return CommonResponse.of(SuccessCode.CREATED);
-    }
+        FeedLikeResponseDto responseDto = feedLikeService.toggleLike(feedId, userId);
 
-    @DeleteMapping
-    public CommonResponse unlike(@PathVariable Long feedId) {
-        Long userId = 1L;
-        feedLikeService.deleteLike(feedId, userId);
-        return CommonResponse.of(SuccessCode.DELETED);
+        return CommonResponse.of(SuccessCode.OK, responseDto);
     }
 }
