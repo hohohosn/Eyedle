20222fabb421c1be305fe02c2b43104f7d125ea2
minsik0 <qwew1212@naver.com>
2025-12-12 02:37:03 +0900

    add: FeedBookmarkRepository 코드 추가

--- feed-service/src/main/java/com/feed_service/domain/repository/FeedBookmarkRepository.java
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedBookmarkRepository.java
@@ -3,9 +3,16 @@ package com.feed_service.domain.repository;
 import com.feed_service.domain.model.FeedBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedBookmarkRepository extends JpaRepository<FeedBookmark, Long> {
 
     boolean existsByFeed_IdAndUserId(Long feedId, Long userId);
 
     void deleteByFeed_IdAndUserId(Long feedId, Long userId);
+
+    long countByFeed_Id(Long feedId);
+
+    List<FeedBookmark> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
+
