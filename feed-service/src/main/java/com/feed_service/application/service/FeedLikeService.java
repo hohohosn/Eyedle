0acf70005e10fb5ce84f1b0effd77fd64d9abd14
minsik0 <qwew1212@naver.com>
2025-12-13 00:16:25 +0900

    fix: 좋아요 서비스 오타 수정

--- feed-service/src/main/java/com/feed_service/application/service/FeedLikeService.java
+++ feed-service/src/main/java/com/feed_service/application/service/FeedLikeService.java
@@ -20,17 +20,17 @@ public class FeedLikeService {
     private final FeedRepository feedRepository;
     private final FeedLikeRepository feedLikeRepository;
 
-    public FeedLikeResponseDto toggleLike(Long feedId, Long feedLikeId) {
+    public FeedLikeResponseDto toggleLike(Long feedId, Long userId) {
 
         Feed feed = feedRepository.findById(feedId)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
 
-        boolean exists = feedLikeRepository.existsByFeed_IdAndUserId(feedId, feedLikeId);
+        boolean exists = feedLikeRepository.existsByFeed_IdAndUserId(feedId, userId);
 
         if(exists) {
-            feedLikeRepository.deleteByFeed_IdAndUserId(feedId, feedLikeId);
+            feedLikeRepository.deleteByFeed_IdAndUserId(feedId, userId);
         } else{
-            feedLikeRepository.save(new FeedLike(feed, feedLikeId));
+            feedLikeRepository.save(new FeedLike(feed, userId));
         }
 
         long likeCount = feedLikeRepository.countByFeed_Id(feedId);
