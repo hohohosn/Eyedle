5e528f5822564b5571b99ce104a3e358f7ac767b
minsik0 <qwew1212@naver.com>
2025-12-13 02:28:28 +0900

    move: 패키지 경로 이동

--- feed-service/src/main/java/com/feed_service/infra/user/UserClient.java
+++ feed-service/src/main/java/com/feed_service/infra/user/UserClient.java
@@ -1,7 +1,7 @@
 package com.feed_service.infra.user;
 
-import com.feed_service.infra.user.response.ApiResponseDto;
-import com.feed_service.infra.user.response.UserInfoResponseDto;
+import com.feed_service.infra.user.dto.ApiResponseDto;
+import com.feed_service.infra.user.dto.UserInfoResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
