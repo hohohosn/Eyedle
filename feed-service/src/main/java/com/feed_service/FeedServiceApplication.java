3883754dcc7e371964efaefc79b9185fefc10c02
minsik0 <qwew1212@naver.com>
2025-12-13 02:07:31 +0900

    add: FeedServiceApplication @EnableFeignClients 추가

--- feed-service/src/main/java/com/feed_service/FeedServiceApplication.java
+++ feed-service/src/main/java/com/feed_service/FeedServiceApplication.java
@@ -2,7 +2,9 @@ package com.feed_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.feed_service.external")
 @SpringBootApplication(scanBasePackages = {
 		"com.feed_service",
 		"com.common"
