264029836cb010e5eea9148d7df10342a9579620
minsik0 <qwew1212@naver.com>
2025-12-16 03:19:35 +0900

    add: HeaderAuthenticationFilter 추가

---
+++ feed-service/src/main/java/com/feed_service/infra/HeaderAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.feed_service.infra;
+
+import io.netty.util.internal.StringUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+public class HeaderAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String userIdHeader = request.getHeader("X-User-Id");
+        String roleHeader = request.getHeader("X-User-Role");
+
+        if (StringUtils.hasText(userIdHeader)) {
+            Long userId = Long.valueOf(userIdHeader);
+
+            List<GrantedAuthority> authorities =
+                    StringUtils.hasText(roleHeader)
+                            ? List.of(new SimpleGrantedAuthority(roleHeader))
+                            : List.of();
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            userId,
+                            null,
+                            authorities
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            log.debug("Header 인증 성공: userId={}, role={}", userId, roleHeader);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
