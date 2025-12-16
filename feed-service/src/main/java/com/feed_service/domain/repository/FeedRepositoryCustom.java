a197410c4fd87f4e5398816668fa0366514cd4c9
minsik0 <qwew1212@naver.com>
2025-12-16 15:31:29 +0900

    add: FeedRepositoryCustom 코드 추가

--- feed-service/src/main/java/com/feed_service/domain/repository/FeedRepositoryCustom.java
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedRepositoryCustom.java
@@ -4,6 +4,12 @@ import com.feed_service.domain.model.Feed;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface FeedRepositoryCustom {
+
     Page<Feed> findFeeds(Pageable pageable);
+
+    List<Feed> findRecentFeeds(LocalDateTime since, String keyword);
 }
