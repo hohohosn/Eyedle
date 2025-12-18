53c7fd230989af1cb8b0630f9f7df8aa423d01a3
kimpompu <domathing@naver.com>
2025-12-18 12:46:42 +0900

    fix : trim 추가

--- gateway/src/main/java/com/gateway/filter/JwtAuthenticationFilter.java
+++ gateway/src/main/java/com/gateway/filter/JwtAuthenticationFilter.java
@@ -46,7 +46,7 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
 			}
 
 			// 토큰 추출
-			String token = authorizationHeader.substring(7);
+			String token = authorizationHeader.substring(7).trim();
 
 			try {
 				// 검증 및 파싱
