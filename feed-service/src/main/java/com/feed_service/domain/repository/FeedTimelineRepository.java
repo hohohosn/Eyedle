6b20cf53301e816c9106ec419002a5412f5f1c93
minsik0 <qwew1212@naver.com>
2025-12-19 22:32:20 +0900

    add: FeedTimelineRepository 추가

---
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedTimelineRepository.java
@@ -0,0 +1,14 @@
+package com.feed_service.domain.repository;
+
+import com.feed_service.domain.model.FeedTimeline;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedTimelineRepository extends JpaRepository<FeedTimeline, Long> {
+
+    Page<FeedTimeline> findByUserIdOrderByCreatedAtDesc(
+            Long userId,
+            Pageable pageable
+    );
+}
