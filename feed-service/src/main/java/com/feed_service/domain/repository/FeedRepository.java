fe662eb415a372ff40e6c1a133b1dca36b741f00
minsik0 <qwew1212@naver.com>
2025-12-19 22:33:22 +0900

    add: FeedRepository 코드 추가

--- feed-service/src/main/java/com/feed_service/domain/repository/FeedRepository.java
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedRepository.java
@@ -4,5 +4,8 @@ import com.feed_service.domain.model.Feed;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositoryCustom{
+    List<Feed> findByIdsWithRelations(List<Long> attr0);
 }
