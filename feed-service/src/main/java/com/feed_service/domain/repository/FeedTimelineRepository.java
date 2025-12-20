8b34c004b1a5946038b1d586d1c1bbd9daa63029
minsik0 <qwew1212@naver.com>
2025-12-20 18:35:42 +0900

    fix: FeedTimelineRepository 수정

--- feed-service/src/main/java/com/feed_service/domain/repository/FeedTimelineRepository.java
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedTimelineRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedTimelineRepository extends JpaRepository<FeedTimeline, Long> {
+public interface FeedTimelineRepository extends JpaRepository<FeedTimeline, Long>, FeedTimelineRepositoryCustom{
 
     Page<FeedTimeline> findByUserId(Long userId, Pageable pageable);
 
