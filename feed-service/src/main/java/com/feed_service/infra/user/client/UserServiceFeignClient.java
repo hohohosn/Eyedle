b0a3bfb35f2b9362eae626a289e68c5a407ab5a6
minsik0 <qwew1212@naver.com>
2025-12-21 00:05:03 +0900

    add: UserServiceFeignClient 추가

---
+++ feed-service/src/main/java/com/feed_service/infra/user/client/UserServiceFeignClient.java
@@ -0,0 +1,30 @@
+package com.feed_service.infra.user.client;
+
+import com.feed_service.infra.config.FeignConfig;
+import com.feed_service.infra.user.dto.UserInfoResponseDto;
+import com.user_service.presentation.dto.response.UserInternalResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Set;
+
+@FeignClient(name = "user-service", path = "/internal", configuration = FeignConfig.class
+)
+public interface UserServiceFeignClient {
+
+    //유저 단건 조회
+    @GetMapping("/users/{userId}")
+    UserInfoResponseDto getUser(@PathVariable Long userId);
+
+    //유저 다건 조회
+    @PostMapping("/users/ids")
+    List<UserInfoResponseDto> getUsersByIds(@RequestBody Set<Long> userIds);
+
+    //팔로워 조회 (fan-out용)
+    @GetMapping("/follows/{userId}/followers")
+    List<UserInternalResponse> getFollowers(@PathVariable Long userId);
+}
