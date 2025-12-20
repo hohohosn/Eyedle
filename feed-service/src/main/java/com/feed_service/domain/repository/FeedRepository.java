e8d657954ac9cca22d5f260ed2fdf1144aace219
minsik0 <qwew1212@naver.com>
2025-12-21 00:19:35 +0900

    fix: FeedRepository 코드 수정

--- feed-service/src/main/java/com/feed_service/domain/repository/FeedRepository.java
+++ feed-service/src/main/java/com/feed_service/domain/repository/FeedRepository.java
@@ -3,9 +3,17 @@ package com.feed_service.domain.repository;
 import com.feed_service.domain.model.Feed;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositoryCustom{
-    List<Feed> findByIdsWithRelations(List<Long> attr0);
+    @Query("""
+        select distinct f
+        from Feed f
+        left join fetch f.feedTags
+        where f.id in :ids
+    """)
+    List<Feed> findByIdsWithRelations(@Param("ids") List<Long> ids);
 }
