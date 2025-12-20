86eda3f286e04f68e567baea234e0eeddc38df08
minsik0 <qwew1212@naver.com>
2025-12-20 23:32:02 +0900

    add: UserInternalResponseDto 추가

---
+++ feed-service/src/main/java/com/feed_service/infra/user/dto/UserInternalResponseDto.java
@@ -0,0 +1,13 @@
+package com.feed_service.infra.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInternalResponseDto {
+
+    private Long userId;
+}
