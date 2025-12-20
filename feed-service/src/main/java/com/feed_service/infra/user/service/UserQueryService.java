55a2cc87250a2c9b6de482ba313647e18caaf891
minsik0 <qwew1212@naver.com>
2025-12-20 23:02:43 +0900

    fix: UserQueryService 코드 수정

--- feed-service/src/main/java/com/feed_service/infra/user/service/UserQueryService.java
+++ feed-service/src/main/java/com/feed_service/infra/user/service/UserQueryService.java
@@ -8,6 +8,11 @@ import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class UserQueryService {
@@ -15,10 +20,14 @@ public class UserQueryService {
     private final UserClient userClient;
 
     public UserInfoResponseDto loadUser(Long userId) {
-        try {
-            return userClient.getUser(userId);
-        } catch (FeignException.NotFound e) {
-            throw new CustomException(ErrorCode.NOT_FOUND);
-        }
+        return userClient.getUser(userId);
+    }
+
+    public Map<Long, UserInfoResponseDto> loadUsers(Set<Long> userIds) {
+        return userClient.getUsersByIds(userIds).stream()
+                .collect(Collectors.toMap(
+                        UserInfoResponseDto::getId,
+                        u -> u
+                ));
     }
 }
