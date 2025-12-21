1f237367c1662be11e5c1e4c86f3d764945ae006
guswnee00 <guswnee00@gmail.com>
2025-12-21 23:43:19 +0900

    add: 접속 상태 기록 #109

--- chat-service/src/main/java/com/chatservice/infra/stomp/StompEventListener.java
+++ chat-service/src/main/java/com/chatservice/infra/stomp/StompEventListener.java
@@ -1,6 +1,7 @@
 package com.chatservice.infra.stomp;
 
 import com.chatservice.application.service.ChatService;
+import com.chatservice.infra.client.PresenceClient;
 import java.security.Principal;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -9,9 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+import static com.chatservice.domain.model.OnlineStatus.OFFLINE;
+import static com.chatservice.domain.model.OnlineStatus.ONLINE;
 
 // 스프링과 stomp는 기본적으로 세션관리를 자동(내부적)으로 처리
 // 연결/해제 이벤트를 기록, 연결된 세션 수를 실시간으로 확인할 목적으로 이벤트 리스너 생성 -> 로그, 디버깅 목적
@@ -22,15 +26,23 @@ public class StompEventListener {
 
   private final Set<String> sessions = ConcurrentHashMap.newKeySet();
   private final ChatService chatService;
+  private final PresenceClient presenceClient;
 
   @EventListener
-  public void connectHandle(SessionConnectEvent event) {
+  public void connectHandle(SessionConnectedEvent event) {
 
     StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
     sessions.add(accessor.getSessionId());
 
-    log.info("connect session ID : {}", accessor.getSessionId());
-    log.info("total sessions : {}", sessions.size());
+    Long userId = getUserIDFromAccessor(accessor);
+    if (userId != null) {
+      try {
+        presenceClient.updateStatus(userId, ONLINE);
+        log.info("{} 유저가 온라인 상태입니다. 세션: {}", userId, accessor.getSessionId());
+      } catch (Exception e) {
+        log.error("Presence 서버 온라인 상태 업데이트 실패: {}", sessions.size());
+      }
+    }
   }
 
   @EventListener
@@ -39,8 +51,15 @@ public class StompEventListener {
     StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
     sessions.remove(accessor.getSessionId());
 
-    log.info("disconnect session ID : {}", accessor.getSessionId());
-    log.info("total sessions : {}", sessions.size());
+    Long userId = getUserIDFromAccessor(accessor);
+    if (userId != null) {
+      try {
+        presenceClient.updateStatus(userId, OFFLINE);
+        log.info("{} 유저가 오프라인 상태입니다. 세션: {}", userId, accessor.getSessionId());
+      } catch (Exception e) {
+        log.error("Presence 서버 오프라인 상태 업데이트 실패: {}", sessions.size());
+      }
+    }
   }
 
   @EventListener
@@ -71,6 +90,22 @@ public class StompEventListener {
       chatService.markMessageAsRead(chatRoomId, userId);
 
       log.info("{}가 채팅방 {} 구독", userId, chatRoomId);
+    }
+  }
+
+  private Long getUserIDFromAccessor(StompHeaderAccessor accessor) {
+
+    Principal principal = accessor.getUser();
+
+    if (principal == null) {
+      return null;
+    }
+
+    try {
+      return Long.parseLong(principal.getName());
+    } catch (NumberFormatException e) {
+      log.error("숫자 형식이 아닙니다.: {}", principal.getName());
+      return null;
     }
   }
 }
