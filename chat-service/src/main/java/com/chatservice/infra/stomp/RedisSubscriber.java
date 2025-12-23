cee8023ff76a64e982b6e7f1b776565933331a0b
guswnee00 <guswnee00@gmail.com>
2025-12-23 11:17:46 +0900

    add: 접속 상태 기록을 위한 redis subscriber #109

---
+++ chat-service/src/main/java/com/chatservice/infra/stomp/RedisSubscriber.java
@@ -0,0 +1,43 @@
+package com.chatservice.infra.stomp;
+
+import com.chatservice.presentation.response.PresenceUpdateResDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedisSubscriber implements MessageListener {
+
+  private final RedisTemplate<String, Object> redisTemplate;
+  private final SimpMessagingTemplate simpMessagingTemplate;
+
+  @Override
+  public void onMessage(Message message, byte[] pattern) {
+    try {
+      String publishMessage = redisTemplate.getStringSerializer().deserialize(message.getBody());
+
+      log.info("Redis Publish Message: {}", publishMessage);
+
+      String payload = publishMessage.replace("\"", "");
+      String[] split = payload.split(":");
+
+      if (split.length < 2) {
+        return;
+      }
+
+      Long userId = Long.parseLong(split[0]);
+      String status = split[1];
+
+      simpMessagingTemplate.convertAndSend("/sub/presence", new PresenceUpdateResDto(userId, status));
+    } catch (Exception e) {
+      log.error("Redis 메시지 처리 에러: {}", e.getMessage());
+    }
+  }
+
+}
